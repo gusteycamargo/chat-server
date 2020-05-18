3267a625012c906be59e13a8e7b1c90005ccf5a2
gustaacamargo <camargo25.gustavo@gmail.com>
2020-05-18 19:04:56 -0300

    Cria controller para o usuário

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,20 @@
 'use strict'
 
+const User = use('App/Models/User');
+const Database = use('Database')
 class UserController {
+    async store ({ request }) {
+        const data = request.only(["username", "email", "password"]);    
+        const user = await User.create(data);
+    
+        return user;
+    }
+
+    async index ({ request }) {
+        const user = await User.all();
+    
+        return user;
+    }
 }
 
 module.exports = UserController
